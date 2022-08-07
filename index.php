08df306bab015fcfb80c7c36b4ebd50ac9babeda
shabagu <shabagu@yandex.ru>
2022-08-07 17:15:34 +0300

    lesson 10 complete

--- index.php
+++ index.php
@@ -8,45 +8,16 @@
     <title>selflearn (php)</title>
 </head>
 <body>
-<?php
-    echo 10 . "<br>";
-    echo -301.123 . "<br>";
-    echo 2 * 2 . "<br>";
-    echo 2 / 5 . "<br>";
-    echo 10 % 3 . "<br>";
-    echo 2 + 2 * 2 . "<br>";
-    echo (2 + 2) * 2 . "<br>";
 
-    $number = 1;
-    $number++;
-    echo $number . "<br>";
-    $number--;
-    echo $number . "<br>";
-    $number = $number + 100;
-    echo $number . "<br>";
-    $number += 100;
-    echo $number . "<br>";
-    $number %= 99;
-    echo $number . "<br>";
-
-    echo abs(-100) . "<br>";
-    echo pow(2, 4) . "<br>";
-    echo sqrt(1024) . "<br>";
-    echo max(5, 10, -19, 40) . "<br>";
-    echo min(5, 10, -19, 40) . "<br>";
-    echo round(25.3) . "<br>";
-    echo ceil(9.01) . "<br>";
-    echo floor(12.9) . "<br>";
-    $a = 4;
-    $b = 5;
-    $c = $a . $b;
-    echo $c . "<br>";
-
-
-
-
-
-?>
+<form action="index.php" method="get">
+    Username:<br>
+    <input type="text" name="username"><br>
+    Age:<br>
+    <input type="number" name="age"><br>
+    <input type="submit">
+</form>
+Your username is <?php echo $_GET["username"]; ?> <br>
+Your age is <?php echo $_GET["age"]; ?>
 
 </body>
 </html>
