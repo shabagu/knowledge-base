1750800585b248c61a21c748a4556a820fa83e36
shabagu <shabagu@yandex.ru>
2022-09-07 02:13:42 +0300

    lesson 7 rework

--- lessons/l06.php
+++ lessons/l06.php
@@ -54,10 +54,34 @@
         </div>
     </section>
     <section class="code">
-        <h3>Здесь отображается код</h3>
+        <h3>Variables</h3>
+<pre>
+<span class="orange">&lt;?php</span>
+    <span class="purple">$characterName</span><span class="grey"> = </span><span class="green">"Tom"</span><span class="orange">;</span>
+    <span class="purple">$characterAge</span><span class="grey"> = </span><span class="blue">45</span><span class="orange">;</span>
+    <span class="orange">echo </span><span class="green">"<span class="grey">There once was a man named <span class="purple">$characterName</span><span class="yellow"> &lt;br&gt;</span></span>"</span>
+    <span class="orange">echo </span><span class="green">"<span class="grey">He was <span class="purple">$characterAge</span> years old<span class="yellow"> &lt;br&gt;</span></span>"</span>
+
+    <span class="purple">$characterName</span><span class="grey"> = </span><span class="green">"John"</span><span class="orange">;</span>
+    <span class="purple">$characterAge</span><span class="grey"> = </span><span class="blue">60</span><span class="orange">;</span>
+    <span class="orange">echo </span><span class="green">"<span class="grey">He rally liked the name <span class="purple">$characterName</span><span class="yellow"> &lt;br&gt;</span></span>"</span>
+    <span class="orange">echo </span><span class="green">"<span class="grey">But didn't like being <span class="purple">$characterAge</span><span class="yellow"> &lt;br&gt;</span></span>"</span>
+<span class="orange">?&gt;</span>
+</pre>
     </section>
     <section class="result">
-        <h3>Здесь отображается результат</h3>
+        <h3>http://localhost:4000</h3>
+        <?php
+            $characterName = "Tom";
+            $characterAge = 45;
+            echo "There once was a man named $characterName <br>";
+            echo "He was $characterAge years old <br>";
+
+            $characterName = "John";
+            $characterAge = 60;
+            echo "He really liked the name $characterName <br>";
+            echo "But didn't like being $characterAge <br>";
+        ?>
     </section>
 </main>
 </body>
--- lessons/l07.php
+++ lessons/l07.php
@@ -54,10 +54,67 @@
         </div>
     </section>
     <section class="code">
-        <h3>Здесь отображается код</h3>
+        <h3>Data types</h3>
+<pre>
+<span class="orange">&lt;?php</span>
+    <span class="purple">$stringVar</span><span class="grey"> = </span><span class="green">"My name is Jeff"</span><span class="orange">;</span>
+    <span class="purple">$integerVar</span><span class="grey"> = </span><span class="blue">123</span><span class="orange">;</span>
+    <span class="purple">$floatVar</span><span class="grey"> = </span><span class="blue">10.5</span><span class="orange">;</span>
+    <span class="purple">$booleanVar</span><span class="grey"> = </span><span class="orange">true;</span>
+    <span class="purple">$nullVar</span><span class="grey"> = </span><span class="orange">null;</span>
+
+    <span class="darkgrey">// array</span>
+    <span class="darkgrey">// object</span>
+    <span class="darkgrey">// resource</span>
+    <span class="darkgrey">// mixed</span>
+
+    <span class="darkgrey">// Имя класса/интерфейса</span>
+    <span class="darkgrey">// self</span>
+    <span class="darkgrey">// parent</span>
+    <span class="darkgrey">// iterable</span>
+    <span class="darkgrey">// callable</span>
+<span class="orange">?&gt;</span>
+
+</pre>
     </section>
     <section class="result">
-        <h3>Здесь отображается результат</h3>
+        <h3>Типы данных</h3>
+        <?php
+            $stringVar = "My name is Jeff";
+            $integerVar = 123;
+            $floatVar = 10.5;
+            $booleanVar = true;
+            $nullVar = null;
+
+            // array
+            // object
+            // resource
+            // mixed
+
+            // Имя класса/интерфейся
+            // self
+            // parent
+            // iterable
+            // callable
+        ?>
+        <ol>
+            <li>String</li>
+            <li>Integer</li>
+            <li>Float (Double)</li>
+            <li>Boolean</li>
+            <li>Null</li>
+            <hr>
+            <li>Array</li>
+            <li>Object</li>
+            <li>Resource</li>
+            <li><a href="https://www.php.net/manual/ru/language.types.declarations.php#language.types.declarations.mixed" target="_blank">Mixed</a></li>
+            <hr>
+            <li>Имя класса/интерфейса</li>
+            <li>Self</li>
+            <li>Parent</li>
+            <li><a href="https://www.php.net/manual/ru/language.types.iterable.php" target="_blank">Iterable</a></li>
+            <li><a href="https://www.php.net/manual/ru/language.types.callable.php" target="_blank">Callable</a></li>
+        </ol>
     </section>
 </main>
 </body>
