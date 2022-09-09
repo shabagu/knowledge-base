4e708d0c197071184d19099731c8fe938165333f
shabagu <shabagu@yandex.ru>
2022-09-09 17:22:24 +0300

    lesson 19 rework

--- lessons/l19.php
+++ lessons/l19.php
@@ -55,11 +55,50 @@
     </section>
     <section class="code">
         <h3>Return Statements</h3>
+<pre>
+<span class="yellow">&lt;form <span class="grey">action=<span class="lightgreen">""</span></span>&gt;</span>
+    <span class="yellow">&lt;label&gt;<span class="grey">Введите число:</span> &lt;input <span class="grey">type=<span class="lightgreen">"number"</span> name=<span class="lightgreen">"number"</span></span>&gt;&lt;/label&gt;</span>
+    <span class="yellow">&lt;input <span class="grey">type=<span class="lightgreen">"submit"</span></span>&gt;</span>
+<span class="yellow">&lt;/form&gt;</span>
+
+<span class="orange">&lt;?php</span>
+    <span class="orange">function <span class="yellow">cube<span class="grey">(<span class="purple">$number</span>) {</span></span></span>
+        <span class="orange">return <span class="grey">pow(<span class="purple">$number</span></span>, <span class="blue">3<span class="grey">)</span></span>;</span>
+        <span class="darkgrey"># return можно использовать для прерывания функции</span>
+        <span class="darkgrey">// echo "test"; # Код после return не выполняется</span>
+    <span class="grey">}</span>
+
+    <span class="grey"><span class="purple">$number</span> = <span class="purple">$_GET</span>[<span class="green">"number"</span>]</span><span class="orange">;</span>
+    <span class="grey"><span class="purple">$result</span> = cube(<span class="purple">$number</span>)</span><span class="orange">;</span>
+
+    <span class="orange">if <span class="grey">(<span class="orange">isset</span>(<span class="purple">$_GET</span>[<span class="green">"number"</span>])) {</span></span>
+        <span class="orange">echo <span class="green">"<span class="purple">$number</span>^3 = <span class="purple">$result</span>"</span>;</span>
+    <span class="grey">}</span>
+<span class="orange">?&gt;</span>
+</pre>
     </section>
     <section class="result">
         <h3>http://localhost:4000</h3>
         <div class="result-output">
+            <form action="">
+                <label>Введите число: <input type="number" name="number"></label>
+                <input type="submit">
+            </form>
 
+            <?php
+                function cube($number) {
+                    return pow($number, 3);
+                    # return можно использовать для прерывания функции
+                    // echo "test"; # Код после return не выполняется
+                }
+
+                $number = $_GET["number"];
+                $result = cube($number);
+
+                if (isset($_GET["number"])) {
+                    echo "$number^3 = $result";
+                }
+            ?>
         </div>
     </section>
 </main>
