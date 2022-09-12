0baeba67216203027fdeaa03800dc2b2b5032fe7
shabagu <shabagu@yandex.ru>
2022-09-12 13:16:10 +0300

    l29 done

--- freecodecamp-php-course/l29/index.php
+++ freecodecamp-php-course/l29/index.php
@@ -55,11 +55,54 @@
     </section>
     <section class="code">
         <h3>Classes & Objects</h3>
+<pre>
+<span class="orange">&lt;?php</span>
+    <span class="orange">class <span class="grey">Book {</span></span>
+        <span class="orange">var <span class="purple">$title</span>;</span>
+        <span class="orange">var <span class="purple">$author</span>;</span>
+        <span class="orange">var <span class="purple">$pages</span>;</span>
+    <span class="grey">}</span>
+
+    <span class="purple">$book1 <span class="grey">= <span class="orange">new <span class="grey">Book()</span>;</span></span></span>
+    <span class="purple">$book1<span class="grey">-></span>title <span class="grey">= <span class="green">"Flow My Tears, the Policeman Said"</span></span></span><span class="orange">;</span>
+    <span class="purple">$book1<span class="grey">-></span>author <span class="grey">= <span class="green">"Philip K. Dick"</span></span></span><span class="orange">;</span>
+    <span class="purple">$book1<span class="grey">-></span>pages <span class="grey">= <span class="blue">231</span></span></span><span class="orange">;</span>
+
+    <span class="purple">$book2 <span class="grey">= <span class="orange">new <span class="grey">Book()</span>;</span></span></span>
+    <span class="purple">$book2<span class="grey">-></span>title <span class="grey">= <span class="green">"The Lord of the Rings"</span></span></span><span class="orange">;</span>
+    <span class="purple">$book2<span class="grey">-></span>author <span class="grey">= <span class="green">"J. R. R. Tolkien"</span></span></span><span class="orange">;</span>
+    <span class="purple">$book2<span class="grey">-></span>pages <span class="grey">= <span class="blue">1178</span></span></span><span class="orange">;</span>
+<span class="orange">?&gt;</span>
+
+<span class="yellow">&lt;pre&gt;<span class="orange">&lt;?php <span class="grey">var_dump(<span class="purple">$book1</span>)</span>;?&gt;</span>&lt;/pre&gt;</span>
+<span class="yellow">&lt;pre&gt;<span class="orange">&lt;?php <span class="grey">var_dump(<span class="purple">$book2</span>)</span>;?&gt;</span>&lt;/pre&gt;</span>
+</pre>
     </section>
     <section class="result">
         <h3>http://localhost:4000</h3>
         <div class="result-output">
-            
+            <?php
+                class Book {
+                    var $title;
+                    var $author;
+                    var $pages;
+                }
+
+                $book1 = new Book();
+                $book1->title = "Flow My Tears, the Policeman Said";
+                $book1->author = "Philip K. Dick";
+                $book1->pages = 231;
+
+                $book2 = new Book();
+                $book2->title = "The Lord of the Rings";
+                $book2->author = "J. R. R. Tolkien";
+                $book2->pages = 1178;
+            ?>
+
+        <pre><?php var_dump($book1);?></pre>
+        <pre><?php var_dump($book2);?></pre>
+
+
         </div>
     </section>
 </main>
