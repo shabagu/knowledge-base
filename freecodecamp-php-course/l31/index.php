57ce62fa62b8bf589151adcbe511e075e95ad3c8
shabagu <shabagu@yandex.ru>
2022-09-14 13:50:12 +0300

    l31 done

--- freecodecamp-php-course/l31/index.php
+++ freecodecamp-php-course/l31/index.php
@@ -55,11 +55,66 @@
     </section>
     <section class="code">
         <h3>Object Functions</h3>
+<pre>
+<span class="orange">&lt;?php</span>
+    <span class="orange">class <span class="grey">Hero {</span></span>
+        <span class="orange">var <span class="purple">$speciality</span>;</span>
+        <span class="orange">var <span class="purple">$lvl</span>;</span>
+        <span class="orange">var <span class="purple">$name</span>;</span>
+
+        <span class="orange">function <span class="yellow">__construct <span class="grey">(<span class="purple">$speciality <span class="orange">, </span>$lvl <span class="orange">, </span>$name</span>) {</span></span></span>
+            <span class="purple">$this<span class="grey">-&gt;</span>speciality <span class="grey">= </span>$speciality<span class="orange">;</span></span>
+            <span class="purple">$this<span class="grey">-&gt;</span>lvl <span class="grey">= </span>$lvl<span class="orange">;</span></span>
+            <span class="purple">$this<span class="grey">-&gt;</span>name <span class="grey">= </span>$name<span class="orange">;</span></span>
+        <span class="grey">}</span>
+
+        <span class="orange">function <span class="yellow">isHighlvl</span></span><span class="grey">() {</span>
+            <span class="orange">if <span class="grey">(<span class="purple">$this<span class="grey">-&gt;</span>lvl</span> > <span class="blue">80</span>) {</span></span>
+                <span class="orange">return true;</span>
+            <span class="grey">}</span>
+            <span class="orange">return false;</span>
+        <span class="grey">}</span>
+    <span class="grey">}</span>
+
+    <span class="purple">$hero1 <span class="grey">= <span class="orange">new </span>Hero(<span class="green">"priest"<span class="orange">, </span><span class="blue">35<span class="orange">, </span></span>"Nicholas"</span>)</span></span><span class="orange">;</span>
+    <span class="purple">$hero1 <span class="grey">= <span class="orange">new </span>Hero(<span class="green">"warrior"<span class="orange">, </span><span class="blue">90<span class="orange">, </span></span>"Michael"</span>)</span></span><span class="orange">;</span>
+
+    <span class="orange">echo <span class="purple">$hero1<span class="grey">-&gt;</span><span class="yellow">isHighlvl</span></span><span class="grey">() ? <span class="green">"true"</span> : <span class="green">"false"</span></span>;</span>
+    <span class="orange">echo <span class="green">"<span class="yellow">&lt;br&gt;</span>"</span>;</span>
+    <span class="orange">echo <span class="purple">$hero2<span class="grey">-&gt;</span><span class="yellow">isHighlvl</span></span><span class="grey">() ? <span class="green">"true"</span> : <span class="green">"false"</span></span>;</span>
+</pre>
     </section>
     <section class="result">
         <h3>http://localhost:4000</h3>
         <div class="result-output">
+            <?php
+                class Hero {
+                    var $speciality;
+                    var $lvl;
+                    var $name;
 
+                    function __construct ($speciality, $lvl, $name) {
+                        $this->speciality = $speciality;
+                        $this->lvl = $lvl;
+                        $this->name = $name;
+                    }
+
+                    function isHighlvl() {
+                         if ($this->lvl > 80) {
+                             return true;
+                         }
+                         return false;
+                    }
+                }
+
+                $hero1 = new Hero("priest", 35, "Nicholas");
+                $hero2 = new Hero("warrior", 90, "Michael");
+
+                echo $hero1->isHighlvl() ? "true" : "false";
+                echo "<br>";
+                echo $hero2->isHighlvl() ? "true" : "false";
+
+            ?>
         </div>
     </section>
 </main>
